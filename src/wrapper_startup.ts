7d134e5c609569dd9c0d7c4901e1997a85d48cd7
Zachary Lund <zachary.lund@streamlabs.com>
2017-08-03 10:17:32 -0700

    Change planned assertions to match previous change

--- src/wrapper_startup.ts
+++ src/wrapper_startup.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 import test from 'ava';
 
 test('wrapper for startup and shutdown', async t => {
-    t.plan(4);
+    t.plan(5);
 
     await startup_shutdown(t, (t) => {
         t.is(obs.ObsGlobal.initialized, true);
